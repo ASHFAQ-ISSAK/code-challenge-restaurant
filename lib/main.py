fe5edcfe023cc422e348b832de37d44f4242a0fc
ASHFAQ ISSAK <ashfaq.abdi@gmail.com>
2023-05-29 08:55:39 +0300

    added hiphen to separate print statements

--- lib/main.py
+++ lib/main.py
@@ -15,52 +15,52 @@ customer1.add_review(restaurant2, 5)
 customer2.add_review(restaurant1, 3)
 
 # Test the methods
-print("--- Testing Customer methods ---")
+print("-------------- Testing Customer methods --------------")
 print("")
 
 # full_name()
 print("Customer 1 full name:")
 print(customer1.full_name())  # Output: John Doe
-print("---")
+print("-------------------------------------------")
 
 # restaurants()
 print("Customer 1 reviewed restaurants:")
 print([restaurant.name for restaurant in customer1.restaurants()])
 # Output: ['Delicious Eatery', 'Tasty Bistro']
-print("---")
+print("-------------------------------------------")
 
 # reviews()
 print("Ratings for Restaurant 1:")
 print([review.rating for review in restaurant1.reviews])
 # Output: [4, 5]
-print("---")
+print("-------------------------------------------")
 
 # customers()
 print("Customers who reviewed Restaurant 1:")
 print([customer.full_name() for customer in restaurant1.customers()])
 # Output: ['John Doe', 'Jane Smith']
-print("---")
+print("-------------------------------------------")
 
 # num_reviews()
 print("Number of reviews by Customer 1:")
 print(Customer.num_reviews(customer1))  # Output: 2
-print("---")
+print("-------------------------------------------")
 
 # find_by_name()
 print("Finding customer by name 'John Doe':")
 found_customer = Customer.find_by_name("John Doe")
 print(found_customer.full_name())  # Output: John Doe
-print("---")
+print("-------------------------------------------")
 
 # find_all_by_given_name()
 print("Finding customers with given name 'Jane':")
 customers_with_given_name = Customer.find_all_by_given_name("Jane")
 print([customer.full_name() for customer in customers_with_given_name])
 # Output: ['Jane Smith']
-print("---")
+print("-------------------------------------------")
 
 # average_star_rating()
 print("Average star rating for Restaurant 1:")
 average_rating = restaurant1.average_star_rating()
 print(average_rating)  # Output: 4.5
-print("---")
+print("-------------------------------------------")
